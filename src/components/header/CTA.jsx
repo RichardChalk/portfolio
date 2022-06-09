b1ce61ab56187b1e6ec642bddecefbbc0ea6387d
Richard Chalk <barmybrit@gmail.com>
2022-06-09 14:41:26 +0200

    Added dummy portrait and social icons

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,10 +4,12 @@ import CV from "../../assets/cv.pdf";
 export const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download>
+      <a href={CV} download className="btn">
         Download CV
       </a>
-      <a href="#contact">Contact me</a>
+      <a href="#contact" className="btn btn-primary">
+        Contact me
+      </a>
     </div>
   );
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
+import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
@@ -10,6 +12,16 @@ const Header = () => {
         <h1>Richard</h1>
         <h5 className="text-light">.NET Fullstack Developer</h5>
         <CTA />
+
+        <div className="me">
+          <img src={ME} alt="Portrait of Richard Chalk" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+
+        <HeaderSocials />
       </div>
     </header>
   );

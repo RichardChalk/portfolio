fed1755c0837c4cabe81fbc92ae7a32822ec94a4
Richard Chalk <barmybrit@gmail.com>
2022-06-09 13:59:30 +0200

    Added title and download cv in header

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CV from "../../assets/cv.pdf";
+
+export const CTA = () => {
+  return (
+    <div className="cta">
+      <a href={CV} download>
+        Download CV
+      </a>
+      <a href="#contact">Contact me</a>
+    </div>
+  );
+};
+
+export default CTA;

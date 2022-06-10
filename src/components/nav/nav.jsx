40bd2995bebf8e6a3e792149dbb7b63fcc628c69
Richard Chalk <barmybrit@gmail.com>
2022-06-10 09:21:07 +0200

    fixed skew bug in navbar icons because of tooltip

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -12,56 +12,56 @@ const Nav = () => {
 
   return (
     <nav>
-      <a
-        href="#"
-        onClick={() => setActiveNav("#")}
-        className={activeNav === "#" ? "active" : ""}
-      >
-        <div className="tooltip">
+      <div className="tooltip">
+        <a
+          href="#"
+          onClick={() => setActiveNav("#")}
+          className={activeNav === "#" ? "active" : ""}
+        >
           <AiOutlineHome />
           <span className="tooltiptext">Home</span>
-        </div>
-      </a>
-      <a
-        href="#about"
-        onClick={() => setActiveNav("#about")}
-        className={activeNav === "#about" ? "active" : ""}
-      >
-        <div className="tooltip">
+        </a>
+      </div>
+      <div className="tooltip">
+        <a
+          href="#about"
+          onClick={() => setActiveNav("#about")}
+          className={activeNav === "#about" ? "active" : ""}
+        >
           <AiOutlineUser />
           <span className="tooltiptext">About Me</span>
-        </div>
-      </a>
-      <a
-        href="#experience"
-        onClick={() => setActiveNav("#experience")}
-        className={activeNav === "#experience" ? "active" : ""}
-      >
-        <div className="tooltip">
+        </a>
+      </div>
+      <div className="tooltip">
+        <a
+          href="#experience"
+          onClick={() => setActiveNav("#experience")}
+          className={activeNav === "#experience" ? "active" : ""}
+        >
           <FiBookOpen />
           <span className="tooltiptext">Experience</span>
-        </div>
-      </a>
-      <a
-        href="#services"
-        onClick={() => setActiveNav("#services")}
-        className={activeNav === "#services" ? "active" : ""}
-      >
-        <div className="tooltip">
+        </a>
+      </div>
+      <div className="tooltip">
+        <a
+          href="#services"
+          onClick={() => setActiveNav("#services")}
+          className={activeNav === "#services" ? "active" : ""}
+        >
           <RiServiceLine />
           <span className="tooltiptext">Services</span>
-        </div>
-      </a>
-      <a
-        href="#contact"
-        onClick={() => setActiveNav("#contact")}
-        className={activeNav === "#contact" ? "active" : ""}
-      >
-        <div className="tooltip">
+        </a>
+      </div>
+      <div className="tooltip">
+        <a
+          href="#contact"
+          onClick={() => setActiveNav("#contact")}
+          className={activeNav === "#contact" ? "active" : ""}
+        >
           <BiMessage />
           <span className="tooltiptext">Contact</span>
-        </div>
-      </a>
+        </a>
+      </div>
     </nav>
   );
 };

2faeeda7f45b7ca0fb8283aa9ca85bba95bdf0d3
Richard Chalk <barmybrit@gmail.com>
2022-06-10 11:16:24 +0200

    Fixed bug with social tooltip

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,18 +6,27 @@ import { FaGithub } from "react-icons/fa";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a
-        href="https://www.linkedin.com/in/richard-erdos-chalk-90853868"
-        target="_blank"
-      >
-        <BsLinkedin />
-      </a>
-      <a href="https://github.com/RichardChalk" target="_blank">
-        <FaGithub />
-      </a>
-      <a href="https://www.facebook.com/richard.chalk.3/" target="_blank">
-        <BsFacebook />
-      </a>
+      <div class="tooltip-social">
+        <a
+          href="https://www.linkedin.com/in/richard-erdos-chalk-90853868"
+          target="_blank"
+        >
+          <BsLinkedin />
+          <span className="tooltiptext-social">Facebook</span>
+        </a>
+      </div>
+      <div class="tooltip-social">
+        <a href="https://github.com/RichardChalk" target="_blank">
+          <FaGithub />
+          <span className="tooltiptext-social">Github</span>
+        </a>
+      </div>
+      <div class="tooltip-social">
+        <a href="https://www.facebook.com/richard.chalk.3/" target="_blank">
+          <BsFacebook />
+          <span className="tooltiptext-social">Facebook</span>
+        </a>
+      </div>
     </div>
   );
 };

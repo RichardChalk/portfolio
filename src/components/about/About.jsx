f4a8c5475b72a39b71fe8734b74036caa9a795d8
Richard Chalk <barmybrit@gmail.com>
2022-06-10 19:44:40 +0200

    Finished with about me and fixes to spacing

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import ME from "../../assets/me-about.jpg";
+import ME from "../../assets/me-about.JPG";
 import { FiAward } from "react-icons/fi";
 import { FiUsers } from "react-icons/fi";
 import { VscLibrary } from "react-icons/vsc";

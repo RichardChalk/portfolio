7623315b836fa6184aee2d0d7a8baeed4d072112
Richard Chalk <barmybrit@gmail.com>
2022-06-10 20:07:44 +0200

    Started experience... frontend

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,8 +1,51 @@
 import React from "react";
 import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
+import HTMLLogo from "../../assets/HTML5_Badge.png";
+import CSSLogo from "../../assets/CSS3_Badge.png";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>What skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <img src={HTMLLogo}></img>
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <img src={CSSLogo}></img>
+              <h4>CSS</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Javascript</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>Bootstrap</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>React</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience__backend"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;

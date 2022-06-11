4934304ab62b42d68dc983e2c0a5ec20f5bee54d
Richard Chalk <barmybrit@gmail.com>
2022-06-11 12:27:58 +0200

    Added 3 services cards

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,80 @@
 import React from "react";
 import "./services.css";
+import { AiOutlineCheckCircle } from "react-icons/ai";
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        {/* Card 1 - UI/UX */}
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <AiOutlineCheckCircle className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* Card 2 - Web Deveoplment */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Web development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <AiOutlineCheckCircle className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* Card 3 - Content creation */}
+        <article className="service">
+          <div className="service__head">
+            <h3>Content creation</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <AiOutlineCheckCircle className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;

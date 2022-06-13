2f3172d570c7201cf169049e677296d361c38406
Richard Erdös Chalk <richard.erdos-chalk@sweetsystems.se>
2022-06-13 11:15:06 +0200

    started contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./contact.css";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsTelephone } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return <section id="contact">
+    <h5>Get in touch</h5>
+    <h2>Contact me</h2>
+
+    <div className="container contact__container">
+      
+      {/* LEFT SIDE - Contact options ================================== */}
+      <div className="contact__options">
+        <article className="contact__option">
+          <HiOutlineMail/>
+          <h4>Email</h4>
+          <h5>richard.erdos.chalk@gmail.com</h5>
+          <a href="mailto:richard.erdos.chalk@gmail.com">Send a message</a>
+        </article>
+        <article className="contact__option">
+          <BsTelephone/>
+          <h4>Telephone</h4>
+          <h5>0046 + 70 469 98 59</h5>
+        </article>
+      </div>
+
+      {/* RIGHT SIDE - Contact form ==================================== */}
+      <form action="">
+
+      </form>
+    </div>
+  </section>;
 };
 
 export default Contact;

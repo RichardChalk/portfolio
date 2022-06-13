2d2074740d6880b24c4f8d3233e1aef37cad59d4
Richard Erdös Chalk <richard.erdos-chalk@sweetsystems.se>
2022-06-13 11:46:27 +0200

    Added 3 real references... need 1 more

--- src/components/testimonials/References.jsx
+++ src/components/testimonials/References.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import AVTR1 from "../../assets/avatar1.jpg"
-import AVTR2 from "../../assets/avatar2.jpg"
-import AVTR3 from "../../assets/avatar3.jpg"
+import AVTR1 from "../../assets/BjornSoderin.jpg"
+import AVTR2 from "../../assets/PeterGotenstedt.PNG"
+import AVTR3 from "../../assets/StefanHolmberg.jpg"
 import AVTR4 from "../../assets/avatar4.jpg"
 
 // PAGINATION PAGINATION PAGINATION PAGINATION PAGINATION
@@ -24,25 +24,25 @@ import 'swiper/css/pagination';
 
 const referencesArray = [
   {
+    id: 3,
+    image: AVTR3,
+    name: "Stefan Holmberg",
+    position: "CEO, Systementor AB",
+    review: "I have never met someone as prestigeless and willing to learn as Richard. He has therefore become an excellent developer in just a few short years. Great personality and atttiude!",
+  },
+  {
     id: 1,
     image: AVTR1,
-    name: "Reference Name 1",
-    position: "VD, Ipsos",
-    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos qui aspernatur enim explicabo doloribus eligendi minima temporibus, eveniet nemo. Reprehenderit dolorum hic doloremque deserunt nobis esse ipsam culpa dolorem quis.",
+    name: "Björn Söderin",
+    position: "CEO, Kantar Millward Brown AB",
+    review: "Richard is a trusted and very appreciated colleague who has contributed to a very pleasant atmosphere at the office and is always there when someone asks for help",
   },
   {
     id: 2,
     image: AVTR2,
-    name: "Reference Name 2",
-    position: "CEO, Synovate",
-    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos qui aspernatur enim explicabo doloribus eligendi minima temporibus, eveniet nemo. Reprehenderit dolorum hic doloremque deserunt nobis esse ipsam culpa dolorem quis.",
-  },
-  {
-    id: 3,
-    image: AVTR3,
-    name: "Reference Name 3",
-    position: "VD, Sweet systems",
-    review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos qui aspernatur enim explicabo doloribus eligendi minima temporibus, eveniet nemo. Reprehenderit dolorum hic doloremque deserunt nobis esse ipsam culpa dolorem quis.",
+    name: "Peter Götenstedt",
+    position: "Global Consultant Director, Universum AB",
+    review: "Richard has been a greatly liked colleauge who has always taken care of his colleagues and his clients. I highly recommend Richard and congratulate in advance anyone that gets him as a new co-worker!",
   },
   {
     id: 4,

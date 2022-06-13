4d42cfd504bda362eaa117630c420a98c5efd9a3
Richard Chalk <barmybrit@gmail.com>
2022-06-13 21:27:14 +0200

    finished footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./footer.css";
+import { BsLinkedin } from "react-icons/bs";
+import { BsFacebook } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        MY LOGO
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a
+          href="https://www.linkedin.com/in/richard-erdos-chalk-90853868"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/RichardChalk" target="_blank">
+          <FaGithub />
+        </a>
+        <a href="https://www.facebook.com/richard.chalk.3/" target="_blank">
+          <BsFacebook />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Richard Chalk</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

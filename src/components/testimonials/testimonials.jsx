4c71210c67c452fd0e6053f46afcc87ca63c836a
Richard Erdös Chalk <richard.erdos-chalk@sweetsystems.se>
2022-06-13 09:33:20 +0200

    First commit from work laptop

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -4,7 +4,7 @@ import "./testimonials.css";
 const Testimonials = () => {
   return (
     <section id="testimonials">
-      <h5>My references</h5>
+      <h5>My references 2</h5>
       <h2>Testimonials</h2>
     </section>
   );
